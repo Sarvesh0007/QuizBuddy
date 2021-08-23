aa539334df5e337464a14fc1dae678c53c2fca19
Sarvesh0007 <sarveshmm99@gmail.com>
2021-08-23 07:55:45 +0530

    bug01

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const app = express();
 connectDb();
 
 // view engine setup
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "/views"));
 app.set("view engine", "ejs");
 
 app.use(expressLayouts);
@@ -32,7 +32,7 @@ app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "/public")));
 
 // session i.e express-session
 app.use(
